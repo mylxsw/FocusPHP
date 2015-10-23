77a20bbccdb1677f2ec7d5bf170b918bc84861c8
管宜尧 <mylxsw@126.com>
2015-10-23 17:03:43 +0800

    add unittest

---
+++ tests/Focus/ContainerTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * FocusPHP
+ *
+ * @link      http://aicode.cc/
+ * @copyright 管宜尧 <mylxsw@aicode.cc>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT (see the
+ *            LICENSE file)
+ */
+
+namespace Focus;
+
+
+class ContainerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHello()
+    {
+        $this->assertTrue(true);
+    }
+}
