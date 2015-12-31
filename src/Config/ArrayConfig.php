fdb3e842b07b83d0a4e7c6b4adca5428da5cab87
管宜尧 <guanyiyao@yunsom.com>
2015-12-31 11:52:05 +0800

    update

--- src/Config/ArrayConfig.php
+++ src/Config/ArrayConfig.php
@@ -4,42 +4,66 @@
  *
  * @link      http://aicode.cc/
  * @copyright 管宜尧 <mylxsw@aicode.cc>
- * @license   http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT (see the
+ *            LICENSE file)
  */
 
 namespace Focus\Config;
 
 
-class ArrayConfig implements Config {
+class ArrayConfig implements Config
+{
     private $_filenames = [];
-    private $_configs = [];
+    private $_configs   = [];
 
-    public function __construct(...$filenames) {
+    public function __construct(...$filenames)
+    {
         $this->_filenames = $filenames;
         $this->reload();
     }
 
-    public function get( $key, $default = null ) {
-        return isset($this->_configs[$key]) ? $this->_configs[$key] : $default;
+    /**
+     * Get config info
+     *
+     * @param string $key
+     * @param mixed  $default
+     *
+     * @return null|string
+     */
+    public function get($key, $default = null)
+    {
+
+        if ( ! isset($this->_configs[$key])) {
+            $cfg = get_cfg_var($key);
+            if ($cfg !== false) {
+                return $cfg;
+            }
+
+            return $default;
+        }
+
+        return $this->_configs[$key];
     }
 
-    public function set( $key, $value ) {
+    public function set($key, $value)
+    {
         $this->_configs[$key] = $value;
     }
 
-    public function reload() {
+    public function reload()
+    {
         if (empty($this->_filenames)) {
             return;
         }
 
         $this->_configs = [];
         foreach ($this->_filenames as $filename) {
-            if (!file_exists($filename)) {
+            if ( ! file_exists($filename)) {
                 throw new \ErrorException("CONFIG_FILE_NOT_FOUND");
             }
 
             $configs = include $filename;
-            if (!is_array($configs)) {
+            if ( ! is_array($configs)) {
                 throw new \DomainException('INVALID_CONFIG_FORMAT');
             }
 
